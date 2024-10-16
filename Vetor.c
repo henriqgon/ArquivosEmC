64714e95ff213782e59004601f3fd0013894d3ab
Henrique Gonçalves Oliveira <emailprofissional.henrique@gmail.com>
2024-10-15 21:52:43 -0300

    Add files via upload

---
+++ Vetor.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+
+    int n;
+    scanf("%d", &n);
+
+    double notas[n];
+
+    for(int i = 0; i < n; i++){
+        scanf("%lf", &notas[i]);
+    }
+
+    double soma = 0;
+    for(int i = 0; i < n; i++){
+
+        soma += notas[i];
+    }
+
+    double media = soma / n;
+
+    printf("%.2lf", media);
+
+    return 0;
+}
